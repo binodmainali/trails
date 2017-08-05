aaf58c68b5b4edd4c92b554d784c513573bc4420
Binod Mainali <binodmainali1@gmail.com>
2017-08-05 13:42:43 +0530

    Added controller support

--- lib/trails.rb
+++ lib/trails.rb
@@ -1,11 +1,24 @@
 require "trails/version"
-require "trails/array.rb"
+require "trails/array"
+require "trails/routing"
 
 module Trails
   class Application
     def call(env)
-         `echo debug > debug.txt`;
-        [200, {'Content-Type' => 'text/html'}, ["Lets go for a trail"] ]
+        klass, action = get_controller_and_action(env)
+        controller = klass.new(env)
+        body = controller.send(action)
+        [200, {'Content-Type' => 'text/html'}, [body] ]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
