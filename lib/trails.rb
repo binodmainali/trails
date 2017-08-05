1ce38e40b62d1aaeb4de3223be81331fb6cf6040
Binod Mainali <binodmainali1@gmail.com>
2017-08-05 12:26:47 +0530

    rack output from gem

--- lib/trails.rb
+++ lib/trails.rb
@@ -1,5 +1,11 @@
 require "trails/version"
+require "trails/array.rb"
 
 module Trails
-  # Your code goes here...
+  class Application
+    def call(env)
+         `echo debug > debug.txt`;
+        [200, {'Content-Type' => 'text/html'}, ["Lets go for a trail"] ]
+    end
+  end
 end
