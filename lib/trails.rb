b39c9bbc58b1fb348a607405e40eac68ff778aab
Binod Mainali <binodmainali1@gmail.com>
2017-08-05 15:19:39 +0530

    redirect to google on root path and custom error message

--- lib/trails.rb
+++ lib/trails.rb
@@ -5,10 +5,25 @@ require "trails/routing"
 module Trails
   class Application
     def call(env)
-        klass, action = get_controller_and_action(env)
-        controller = klass.new(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      elsif env['PATH_INFO'] == '/'
+        return [ 302, {'Location' =>"http://google.com"}, [] ]
+      end
+      klass, action = get_controller_and_action(env)
+      controller = klass.new(env)
+      begin
         body = controller.send(action)
         [200, {'Content-Type' => 'text/html'}, [body] ]
+      rescue
+        body = "
+          <div>
+            <h1>Oops!</h1>
+            <h2>We could't find the page <br>you were looking for.</h2>
+            <p>You may have mistyped the address or the page may have moved.</p>
+          </div>"
+        [200, {'Content-Type' => 'text/html'}, [body] ]
+      end
     end
   end
 
